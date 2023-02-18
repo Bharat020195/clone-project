dceaf054ad5e057d6e38856c11baa317fa67ecba
Bharat020195 <105201445+Bharat020195@users.noreply.github.com>
2023-02-18 08:07:22 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,11 +6,11 @@ import Webskite from '../images/Websbite logo.png'
 export default function header() {
   return (
     <div id='HH'>
-     <Link to='index'><img src={Webskite} id='HHimg'/></Link>
-     <Link to='Approach' id='Hap'>Approach</Link>
-     <Link to='Edge' id='Hed'>Edge</Link>
-     <Link to='About' id='Hab'>About</Link>
-     <Link to='Letstalk'><button id='Hle'>Lets Talk</button></Link>
+     <Link to='/index'><img src={Webskite} id='HHimg'/></Link>
+     <Link to='/Approach' id='Hap'>Approach</Link>
+     <Link to='/Edge' id='Hed'>Edge</Link>
+     <Link to='/About' id='Hab'>About</Link>
+     <Link to='/Letstalk'><button id='Hle'>Lets Talk</button></Link>
     </div>
   )
 }
